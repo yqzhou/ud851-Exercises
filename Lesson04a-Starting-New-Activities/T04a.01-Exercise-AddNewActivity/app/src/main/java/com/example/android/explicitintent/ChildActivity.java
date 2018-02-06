1d35ef3a39ccd9b12b235d617b8aeef20442c45c
yqzhou <ciao.zyq@gmail.com>
2018-02-05 22:43:03 -0800

    T04a.01 + upgrade gradle

---
+++ Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.explicitintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ChildActivity extends AppCompatActivity {
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        mTextView = (TextView) findViewById(R.id.tv_display);
+    }
+}
