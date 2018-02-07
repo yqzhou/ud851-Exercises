2952c7422e4f9c015baa4b3d2f144b05558db5cb
yqzhou <ciao.zyq@gmail.com>
2018-02-06 22:46:49 -0800

    T04b.01 + update gradle

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,8 +15,12 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -35,10 +39,9 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenWebpageButton(View v) {
-        // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
+        String url = "https://google.com";
 
-        // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        openWebPage(url);
     }
 
     /**
@@ -77,12 +80,11 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-    // TODO (1) Create a method called openWebPage that accepts a String as a parameter
-    // Do steps 2 - 4 within openWebPage
-
-        // TODO (2) Use Uri.parse to parse the String into a Uri
-
-        // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
-
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
+    public void openWebPage(String param) {
+       Uri uri = Uri.parse(param);
+       Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+       if (intent.resolveActivity(getPackageManager()) != null) {
+           startActivity(intent);
+        }
+    }
 }
